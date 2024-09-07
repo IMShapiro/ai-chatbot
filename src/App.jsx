4dc187958de82f30ef28aceccdbe00459cfe61cf
Shepherd <shepherdmhlanga20@gmail.com>
2024-09-07 12:49:37 +0200

    Integrated AI model App.jsx to respond to text prompts and added some basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,53 @@
+import { useState } from "react";
+import { processPrompt } from "./Config/ai-config";
+import "./assets/styles/App.css";
+import micIcon from "./assets/icons/mic-icon.svg";
+import sendIcon from "./assets/icons/send-icon.svg";
+
 function App() {
 
+  const [prompt, setPrompt] = useState("");
+  const [messages,setMessages] = useState([]);
+
+  function handleChange(e){
+    e.preventDefault;
+    setPrompt(e.target.value);
+  }
+
+  async function handleSubmit(){
+    if(prompt){
+      let data = await processPrompt(prompt);
+      setMessages((messages)=>[...messages,{user:prompt,bot:data}])
+      setPrompt(""); 
+    }
+  }
+
   return (
-    <>
-      Hello World
-    </>
+    <div>
+      <h1>Chatter</h1>
+      <div className="chats-container">  
+          {messages.length > 0 && messages.map((item,index)=>{
+              return (
+                <div className="messages" key={index}>
+                    <p className="user">{item.user}</p>
+                    <p className="bot">{item.bot}</p>
+                </div>
+              )
+          })}
+      </div>
+      <div className="input-container">
+        <input 
+          placeholder="Ask me anything..."
+          type="text"
+          id="prompt"
+          name="prompt"
+          value={prompt}
+          onChange={handleChange}
+          className="form-field"
+        />
+        <button className="btn btn-sm m-1 btn-secondary" onClick={handleSubmit}><img src={prompt != "" ? sendIcon: micIcon} alt="send icon"/></button>
+      </div>
+    </div>
   )
 }
 
---
+++ src/Config/ai-config.js
@@ -0,0 +1,21 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+export async function processPrompt(prompt){
+
+    if(!prompt)
+        return "Please enter a prompt"
+
+    let response = "";
+    
+    try{
+        const result = await model.generateContent(prompt);
+        response = result.response.text();
+        console.log(response);
+        return response;
+    }catch(error){
+        console.error(error)
+    }
+}
