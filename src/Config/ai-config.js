43a082e74dff66230c4c803299ccd75f8abbc232
Shepherd <shepherdmhlanga20@gmail.com>
2024-09-11 13:49:51 +0200

    Implemented gemini's chat feature in ai-config so the chatbot retains some context of the converstion

--- src/Config/ai-config.js
+++ src/Config/ai-config.js
@@ -2,12 +2,13 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI(import.meta.env.VITE_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const chat = model.startChat({})
 
 export async function processPrompt(prompt){
     let response = "";
     
     try{
-        const result = await model.generateContent(prompt);
+        const result = await chat.sendMessage(prompt)
         response = result.response.text();
         console.log(response);
         return response;
